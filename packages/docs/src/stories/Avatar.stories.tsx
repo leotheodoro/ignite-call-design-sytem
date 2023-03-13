c6fec38939ee13939fb5caa6a17b23800f63dc47
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-03-13 16:59:09 -0300

    🚸 Components control

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/leotheodoro.png',
     alt: 'Leonardo Theodoro',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -7,6 +7,29 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis aliquid, blanditiis quia quaerat eum provident officia magnam maiores temporibus iure autem at optio aut, dolor pariatur maxime nihil laboriosam placeat?',
+    size: 'xs',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
